1418d07d39ee347e7b042f3cf519654db4f4ff18
ajaysinghpanwar2002 <ajaysingh07032003@gmail.com>
2023-07-03 03:56:49 +0530

    client

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -2,20 +2,26 @@
     <div>
         <h1
             class="w-full flex justify-center text-xl p-10 text-zinc-300 font-medium text-opacity-80 hover:text-opacity-100">
-            Generate Github documentation using LLM</h1>
-        <div class="w-full flex justify-around p-10 ">
+            Generate Github documentation using LLM
+        </h1>
+        <div class="w-full flex justify-around p-10">
             <input class="placeholder-black p-2 rounded-sm" type="text" placeholder="Enter Github Username"
                 v-model="username" />
             <input class="placeholder-black p-2 rounded-sm" type="text" placeholder="Enter Repository Name"
-                v-model="RepositoryName" />
+                v-model="repositoryName" />
         </div>
-        <div class="flex justify-center p-10 ">
-            <button @click="getReadme" type="button" class="bg-gray-50 hover:bg-gray-200 rounded-sm px-2">Make
-                documentation</button>
+        <div class="flex justify-center p-10">
+            <button @click="getReadme" type="button" class="bg-gray-50 hover:bg-gray-200 rounded-sm px-2"
+                :disabled="isInputEmpty">
+                Make documentation
+            </button>
         </div>
         <div class="w-full flex justify-center p-20">
             <template v-if="loading">
-                <loading />
+                <Loading />
+            </template>
+            <template v-else-if="readmeData">
+                <MarkDown :content="readmeData" />
             </template>
             <template v-else>
                 <img src="../assets/GitHub_Invertocat_Logo.svg" />
@@ -28,23 +34,30 @@
 import axios from 'axios';
 import { SERVER_URL } from '../constants';
 import Loading from './Loading.vue';
+import MarkDown from './MarkDown.vue';
 
 export default {
     name: 'Home',
     data() {
         return {
             username: '',
-            RepositoryName: '',
+            repositoryName: '',
             loading: false,
+            readmeData: '',
         };
+    },
+    computed: {
+        isInputEmpty() {
+            return this.username === '' || this.repositoryName === '';
+        },
     },
     methods: {
         async getReadme() {
-            const URL = `${SERVER_URL}/${this.username}/${this.RepositoryName}`;
+            const URL = `${SERVER_URL}/${this.username}/${this.repositoryName}`;
             this.loading = true;
             try {
-                const readmeData = await axios.get(URL);
-                console.log(readmeData.data);
+                const response = await axios.get(URL);
+                this.readmeData = response.data;
             } catch (error) {
                 console.error(error);
             } finally {
@@ -54,6 +67,7 @@ export default {
     },
     components: {
         Loading,
+        MarkDown,
     },
 };
 </script>
---
+++ client/src/components/MarkDown.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="w-full flex justify-center">
+        <div v-html="parsedContent"></div>
+    </div>
+</template>
+
+<script>
+import MarkdownIt from 'markdown-it';
+
+export default {
+    name: "MarkDown",
+    props: {
+        content: {
+            type: String,
+            required: true
+        }
+    },
+    computed: {
+        parsedContent() {
+            const md = new MarkdownIt();
+            return md.render(this.content);
+        }
+    }
+};
+</script>
